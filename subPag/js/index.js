e8711438ca2db110c6b847414a10e393dd338a78
Amara-Siqueira <siqueiraamara6@gmail.com>
2024-09-24 16:17:15 -0300

    att
    atualizaçao do social mudei abordagem

---
+++ subPag/js/index.js
@@ -0,0 +1,7 @@
+const $cards = document.querySelectorAll('.js-card')
+const $menu =  document.querySelector('js-menu')
+const $icon = document.querySelectorALL('js-icon')
+
+$menu.addEventListener('click', e =>){
+    if(e.target.classList.contains())
+}
